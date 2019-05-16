fc7ca4738e8e869c78c12640085a03d303cc935d
samihda <samihda@users.noreply.github.com>
2019-05-16 19:06:43 +0200

    Generate numbers automatically

--- src/main.c
+++ src/main.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "sort.h"
 
-#define LENGTH 3
+#define LENGTH 20
+
+void seedrandom()
+{
+  srand(time(0));
+}
+
+int random50()
+{
+  return rand() % 50;
+}
+
+void printnum(int len, int *list)
+{
+  for (int i = 0; i < len; i++) {
+    printf("%d ", list[i]);
+  }
+
+  printf("\n");
+}
 
 int main()
 {
   int i, j, a, numbers[LENGTH], numberscopy[LENGTH];
 
+  seedrandom();
+
   for (i = 0; i < LENGTH; i++) {
-    printf("Enter number: ");
-    scanf("%d", &numbers[i]);
+    numbers[i] = random50(LENGTH);
   }
+
+  printf("Random numbers:\n");
+  printnum(LENGTH, numbers);
 
   msort(LENGTH, numbers, numberscopy);
 
   printf("Ordered numbers:\n");
 
-  for (i = 0; i < LENGTH; i++) {
-    printf("%d\n", numbers[i]);
-  }
+  printnum(LENGTH, numbers);
 
   return 0;
 }
