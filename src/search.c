dcfd8ffcf197f34f67aff3064b443f4eca9cd495
samihda <samihda@users.noreply.github.com>
2019-05-29 11:23:26 +0200

    Implement binary search

--- src/search.c
+++ src/search.c
@@ -8,3 +8,24 @@ int search(int n, int len, int *list)
 
   return -1;
 }
+
+int bsearch(int n, int len, int *list)
+{
+  int i = 0, j = len - 1;
+
+  while (i <= j) {
+    int k = (i + j) / 2;
+
+    if (list[k] == n) {
+      return k;
+    }
+
+    if (list[k] > n) {
+      j = k - 1;
+    } else {
+      i = k + 1;
+    }
+  }
+
+  return -1;
+}
--- src/search.h
+++ src/search.h
@@ -1,1 +1,2 @@
 int search(int n, int len, int *list);
+int bsearch(int n, int len, int *list);
